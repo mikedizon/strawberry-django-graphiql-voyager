487a49eadd0fdc74d76cfdf96c7838cf525e4645
Michael Dizon <mdizon@gmail.com>
2022-07-31 18:53:13 -0400

    WIP

--- strawberry_django_graphiql_voyager/views.py
+++ strawberry_django_graphiql_voyager/views.py
@@ -7,35 +7,15 @@ from django.template import RequestContext, Template
 from django.template.loader import render_to_string
 from django.template.exceptions import TemplateDoesNotExist
 from django.template.response import TemplateResponse
-from strawberry.django.views import AsyncGraphQLView as GraphQLView
+from django.views.generic import TemplateView as GenericTemplateView
+from django.conf import settings
 
 
-class AsyncGraphQLView(GraphQLView):
-    graphiql_html_file = "strawberry_django_graphiql_voyager/graphiql.html"
+class TemplateView(GenericTemplateView):
+    template_name = "strawberry_django_graphiql_voyager/voyager.html"
 
-    def _render_graphiql(self, request: HttpRequest, context=None):
-        if not self.graphiql:
-            raise Http404()
-        template1 = Template(render_to_string(self.graphiql_html_file))
-        try:
-            template = Template(render_to_string(self.graphiql_html_file))
-        except TemplateDoesNotExist:
-            template = Template(
-                open(
-                    os.path.join(
-                        os.path.dirname(os.path.abspath(strawberry.__file__)),
-                        "static/graphiql.html",
-                    ),
-                    "r",
-                ).read()
-            )
-
-        context = context or {}
-        context.update(
-            {"SUBSCRIPTION_ENABLED": json.dumps(self.subscriptions_enabled)})
-
-        response = TemplateResponse(
-            request=request, template=None, context=context)
-        response.content = template.render(RequestContext(request, context))
-
-        return response
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        graphql_endpoint = settings.GRAPHENE_VOYAGER['GRAPHQL_ENDPOINT']
+        context['graphql_endpoint'] = graphql_endpoint
+        return context
--- strawberry_django_graphiql_voyager/voyager/src/index.js
+++ strawberry_django_graphiql_voyager/voyager/src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Voyager } from 'graphql-voyager';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function introspectionProvider(query) {
+  const { graphqlEndpoint } = window.VOYAGER_SETTINGS
+  console.log({ graphqlEndpoint });
+  return fetch(graphqlEndpoint, {
+    method: 'post',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ query: query }),
+  }).then((response) => response.json());
+}
+
+ReactDOM.render(
+    <Voyager introspection={introspectionProvider} />,
+    document.getElementById('root'),
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
